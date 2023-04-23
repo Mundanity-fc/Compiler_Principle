28651fb9414dc2ee1034c22359536ca7fd2a03d0
Mundanity Fan <i@mundanity.moe>
2023-04-23 17:26:54 +0800

    更新对产生式的处理，但是 NFA 构建的方向似乎搞错了，准备参考《编译原理》

--- lexical_analysis.cpp
+++ lexical_analysis.cpp
@@ -34,21 +34,37 @@ void Analyser::generate() {
         // 忽略空行
         if(line.empty())
             continue;
+        // 将产生式划为左右两部分
         std::vector<std::string> production = this->divide_line(line);
+        // 第一次需添加开始符
         if(!is_get_starter){
-
+            this->G.S = production[0];
+            is_get_starter = true;
         }
+        // 将本行产生式加入到 G 中
+        this->G.P.push_back(line);
+
+        // 以本行产生式建立节点，若节点不在类的 nodes 表中，则将该节点信息加入该表
+        Node node;
+        if (!this->is_created_node(production[0])){
+            node.name = production[0];
+            this->nodes.push_back(node);
+        }
+        Node* current_node = find_node(production[0]);
+        std::vector<std::string> rights = this->split_right(production[1]);
         std::cout << line << std::endl;
     }
     std::cout << std::endl;
 }
 
-std::vector<std::string> Analyser::divide_line(std::string production) {
+std::vector<std::string> Analyser::divide_line(const std::string& production) {
     std::vector<std::string> divided;
     int max_length = production.length();
     int divide = production.find("->");
     std::string leftPart = production.substr(0, divide - 1);
+    this->clean_space(leftPart);
     std::string rightPart = production.substr(divide + 2, max_length);
+    this->clean_space(rightPart);
     divided.push_back(leftPart);
     divided.push_back(rightPart);
     return divided;
@@ -60,4 +76,34 @@ void Analyser::clean_space(std::string &str) {
         str.erase(location,1);
         location = str.find(' ');
     }
+}
+
+bool Analyser::is_created_node(const std::string& name){
+    for (int i = 0; i < this->nodes.size(); ++i) {
+        if (this->nodes[i].name == name){
+            return true;
+        }
+    }
+    return false;
+}
+
+Node* Analyser::find_node(const std::string& name) {
+    for (int i = 0; i < this->nodes.max_size(); ++i) {
+        if (this->nodes[i].name == name){
+            return &this->nodes[i];
+        }
+    }
+    return nullptr;
+}
+
+std::vector<std::string> Analyser::split_right(std::string right) {
+    std::string current_right = right;
+    std::vector<std::string> result;
+    auto location = current_right.find('|');
+    while (location != std::string::npos){
+        result.push_back(current_right.substr(0,location));
+        current_right = current_right.substr(location+1);
+        location = current_right.find('|');
+    }
+    return result;
 }
--- lexical_analysis.h
+++ lexical_analysis.h
@@ -8,14 +8,20 @@
 #include <vector>
 #include <string>
 
-struct Node{
-    std::string name;
+// 边定义
+struct Edge{
+    // 边上符号
+    std::string vn;
+    // 边指向节点名称
+    std::string vt;
 };
 
-struct Edge{
-    Node* prevNode;
-    Node* nextNode;
-    std::string target;
+// 节点定义
+struct Node{
+    // 节点名称
+    std::string name;
+    // 节点包含边
+    std::vector<Edge> edges;
 };
 
 // 文法G的四元组
@@ -30,23 +36,41 @@ struct Grammar{
     std::string S;
 };
 
+// 分析器类定义
 class Analyser{
 private:
     // 文法产生式位置
     std::string grammar_file;
+
     // 文法 G 的参数
     Grammar G;
+
+    // 节点集合
+    std::vector<Node> nodes;
 
 public:
     // 构造函数
     Analyser(std::string filename);
+
     // 析构函数
     ~Analyser();
-    //
+
+    // NFA 生成
     void generate();
+
     // 产生式分割，将产生式划分为左部与右部
-    std::vector<std::string> divide_line(std::string production);
+    std::vector<std::string> divide_line(const std::string& production);
+
+    // 产生式右侧以'|'再次进行分割
+    std::vector<std::string> split_right(std::string right);
+
     //清除多余空格
     void clean_space(std::string &str);
+
+    // 检测类的 nodes 中是否已经存在现有节点
+    bool is_created_node(const std::string& name);
+
+    // 寻找指定名称的节点在 nodes 中的位置
+    Node* find_node(const std::string& name);
 };
 
