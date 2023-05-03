3b52453697756f2c553ba2f3928a7f24dbd9ce48
Mundanity Fan <i@mundanity.moe>
2023-05-04 03:37:11 +0800

    暂时结束

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 #include "grammar_analysis.h"
 
 int main() {
-    std::cout << "词法分析" << std::endl;
+    std::cout << "词法分析：" << std::endl;
     Lexical_Analysis Task1("001_input_lexical_input_grammar.txt");
     Task1.generate();
     Task1.print_NFA();
     Task1.check("001_input_sourcecode.txt");
     Task1.print_result();
     Task1.save_result();
-    std::cout << "语法分析" << std::endl;
+
+    std::cout << "语法分析：" << std::endl;
     Grammar_Analysis Task2;
     if(Task2.check_is_LL1())
         std::cout << "符合LL(1)文法" << std::endl;
